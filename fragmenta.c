bba4ed9c0237916f8910cf76fc858c71c63cde3f
AndreaZ2306 <117767655+AndreaZ2306@users.noreply.github.com>
2025-11-23 01:52:36 +0100

    Update strtok delimiters to include newline character

--- fragmenta.c
+++ fragmenta.c
@@ -13,19 +13,19 @@ char **fragmenta(const char *e) {
     s1 = strdup(e);
     s2 = strdup(e);
 
-    s = strtok(s2, ",");
+    s = strtok(s2, ",\n");
     while (s != NULL) {
-        s = strtok(NULL, ",");
-        tamano_de_tabla = tamano_de_tabla + 1;
+        s = strtok(NULL, ",\n");
+        tamano_de_tabla++;
     }
 
     salida = (char **)malloc((tamano_de_tabla) * sizeof(char *));
 
-    s = strtok(s1, ",");
+    s = strtok(s1, ",\n");
     int i = 0;
     while (s != NULL) {
         salida[i] = strdup(s);
-        s = strtok(NULL, ",");
+        s = strtok(NULL, ",\n");
         i = i + 1;
     }
     salida[tamano_de_tabla - 1] = NULL;
