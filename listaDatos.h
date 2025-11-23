91ba859f315485a90d2fa0211ad5154fae21f1d7
AndreaZ2306 <117767655+AndreaZ2306@users.noreply.github.com>
2025-11-23 02:12:43 +0100

    Refactor listaDatos.h structure and increase array sizes

--- listaDatos.h
+++ listaDatos.h
@@ -1,27 +1,31 @@
+#ifndef LISTADATOS_H
+#define LISTADATOS_H
+
 #include <stdbool.h>
 
 typedef struct Datos
 {
     float tiempo_a_solas;
-    char miedoEscenico [10];
+    char miedoEscenico[20];       
     float frec_asistencia_eventos;
     float frec_salidas;
-    char agotamiento [10];
+    char agotamiento[20];
     float n_amigos_cercanos;
     float frec_publicacion_redes;
-    char resultado [20];
-}Datos;
+    char resultado[30];
+} Datos;
 
 typedef Datos tipoElementoLista;
 
-typedef struct celdaL{
-	tipoElementoLista elem;
-	struct celdaL *sig;
-}celdaLista; 
-typedef struct tipoL{
-	celdaLista *ini;
-	celdaLista *fin;
-}tipoLista;
+typedef struct celdaL {
+    tipoElementoLista elem;
+    struct celdaL *sig;
+} celdaLista;
+
+typedef struct tipoL {
+    celdaLista *ini;
+    celdaLista *fin;
+} tipoLista;
 
 void nuevaLista(tipoLista *);
 
@@ -37,3 +41,4 @@ bool esNulaLista(tipoLista);
 
 void imprimirLista(tipoLista lista);
 
+#endif
